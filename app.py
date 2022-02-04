a0c048c3182e22619084f6d4f4b7570f1f6e4845
Ryan Hennessy <ryan.hennessy@lacework.com>
2022-02-04 09:00:42 -0600

    Updated the function to act on both the GET and POST calls

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def format_call():
-   alertdata=request.json
+   if request.method == 'GET':
+       return "Done"
+   else:
+       alertdata=request.json
+       senddata = {}
+       senddata["customer_id"] = os.environ['CUSTOMER_KEY']
+       senddata["log_type"] = "SOME_LOG"
+       senddata["entries"] = [alertdata]
 
-   senddata = {}
-   senddata["customer_id"] = os.environ['CUSTOMER_KEY']
-   senddata["log_type"] = "SOME_LOG"
-   senddata["entries"] = [alertdata]
-
-   x = requests.post(os.environ['GC_URL'], data=json.dumps(senddata))
-
-   return "All Good"
+       x = requests.post(os.environ['GC_URL'], data=json.dumps(senddata))
+       return "Event Sent"
